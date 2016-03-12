04545f469566db860db1c58fa2be2e59e6159b23
Richard Townsend <richard@sentimentron.co.uk>
2016-03-12 13:07:21 +0000

    That's about 1.5M-2.5M insertions per second

--- Investigations/DB/test_append_speed.py
+++ Investigations/DB/test_append_speed.py
@@ -5,6 +5,7 @@ import postgresql
 import logging
 import time
 import numpy
+from collections import defaultdict
 
 if __name__ == "__main__":
 
@@ -12,30 +13,28 @@ if __name__ == "__main__":
     db.query("drop owned by current_user")
 
     logging.info("Creating database tables...")
-    db.query(r"""CREATE TABLE inverted (id serial, words integer[])""")
-
-    # Create 10 entries in the table
-    ins = db.prepare(r"""INSERT INTO inverted DEFAULT VALUES""")
-    for _ in range(10):
-        ins()
+    db.query(r"""CREATE TABLE inverted (packid serial, keyword integer, words integer[])""")
 
     # Keep a count of how many things we've inserted
     counts = {(i+1) : 0 for i in range(10)}
     def total_count():
         return sum([counts[i] for i in counts])
 
-    append = db.prepare(r"""UPDATE inverted SET words = array_append(words, $2)
-    WHERE id = $1""")
+    append = db.prepare(r"""INSERT INTO inverted (keyword, words) VALUES ($1,
+    $2)""")
 
     # Insert until further notice
     while total_count() < 10E6:
         # Generate a random number between 1 and 10
         keys = numpy.random.randint(1, 11, 10000)
         values = numpy.random.randint(1, None, 10000)
+        insert_plan = defaultdict(list)
+        for k, v in zip(keys, values):
+            insert_plan[k].append(v)
         # Measure the start time
         start = time.time()
-        for k, v in zip(keys, values):
-            append(k, v)
+        for k in insert_plan:
+            append(k, insert_plan[k])
         end = time.time()
         for k, v in zip(keys, values):
             counts[k] = counts[k] + 1
