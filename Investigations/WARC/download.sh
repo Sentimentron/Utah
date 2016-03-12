305ddfa8a3b7a188c3aa0b9ca814fc0e4298cbbc
Richard Townsend <richard@sentimentron.co.uk>
2016-03-12 18:04:44 +0000

    download.sh: download some sample WARC files

---
+++ Investigations/WARC/download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/warc.paths.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462472.19/warc/CC-MAIN-20150226074102-00293-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463378.98/warc/CC-MAIN-20150226074103-00204-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936465599.34/warc/CC-MAIN-20150226074105-00000-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463444.94/warc/CC-MAIN-20150226074103-00011-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462099.15/warc/CC-MAIN-20150226074102-00290-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463104.7/warc/CC-MAIN-20150226074103-00063-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462313.6/warc/CC-MAIN-20150226074102-00107-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463928.32/warc/CC-MAIN-20150226074103-00029-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936461359.90/warc/CC-MAIN-20150226074101-00055-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936461907.65/warc/CC-MAIN-20150226074101-00021-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463608.25/warc/CC-MAIN-20150226074103-00159-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936464123.82/warc/CC-MAIN-20150226074104-00089-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462099.15/warc/CC-MAIN-20150226074102-00322-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463458.93/warc/CC-MAIN-20150226074103-00113-ip-10-28-5-156.ec2.internal.warc.gz
+wget -nc https://aws-publicdatasets.s3.amazonaws.com/common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936464809.62/warc/CC-MAIN-20150226074104-00071-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463093.76/warc/CC-MAIN-20150226074103-00247-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936461995.64/warc/CC-MAIN-20150226074101-00106-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936465693.74/warc/CC-MAIN-20150226074105-00010-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463444.94/warc/CC-MAIN-20150226074103-00193-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462762.87/warc/CC-MAIN-20150226074102-00103-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463637.80/warc/CC-MAIN-20150226074103-00015-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936464809.62/warc/CC-MAIN-20150226074104-00104-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463928.32/warc/CC-MAIN-20150226074103-00100-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936463181.39/warc/CC-MAIN-20150226074103-00054-ip-10-28-5-156.ec2.internal.warc.gz
+#common-crawl/crawl-data/CC-MAIN-2015-11/segments/1424936462577.56/warc/CC-MAIN-20150226074102-00302-ip-10-28-5-156.ec2.internal.warc.gz
